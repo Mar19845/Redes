f73e3f156fd8b5f0ea4b23ff7e12d51278bb7088
Mar19845 <mar19845@uvg.edu.gt>
2022-08-28 10:45:03 -0600

    Update tree.py

--- LAB3/tree.py
+++ LAB3/tree.py
@@ -3,15 +3,16 @@ import networkx as nx
 
 class Tree():
     def __init__(self, topo, usernames):
-        Graph = nx.Graph()
+        self.Graph = nx.Graph()
         # Adding nodes
         for key, value in usernames["config"].items():
-            Graph.add_node(key, jid=value)
+            self.Graph.add_node(key, jid=value)
             
         # Adding edges and assigning different weights
         for key, value in topo["config"].items():
             for i in value:
                 weightA = random.uniform(0, 1)
-                Graph.add_edge(key, i, weight=weightA)
+                self.Graph.add_edge(key, i, weight=weightA)
         
-        return Graph
+    def get_graph(self):
+        return self.Graph
